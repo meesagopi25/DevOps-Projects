ef03e47ea7130336f793a2a5c492ffa216809132
meesagopi25 <meesagopi25@gmail.com>
2026-01-10 17:17:26 +0530

    Update main.tf

--- Day-08/main.tf
+++ Day-08/main.tf
@@ -1,3 +1,7 @@
+
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired region
+}
 resource "aws_instance" "web_server" {
   ami           = "ami-0fc5d935ebf8bc3bc" # Ubuntu 22.04
   instance_type = "t2.micro"
