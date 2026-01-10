c93f3ae8d58bdbc0d9e3bbf3ac6ef20955c9130a
meesagopi25 <meesagopi25@gmail.com>
2026-01-10 16:36:29 +0530

    Create main.tf

---
+++ Day-08/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web_server" {
+  ami           = "ami-0fc5d935ebf8bc3bc" # Ubuntu 22.04
+  instance_type = "t2.micro"
+  key_name      = "my-jenkins-key"
+  tags          = { Name = "Jenkins-Provisioned-Node" }
+}
+
+output "public_ip" {
+  value = aws_instance.web_server.public_ip
+}
